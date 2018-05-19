5697e8ee009cdf0429ecc1eb55e5ba780b57b8b3
league <league@wts14.attlocal.net>
2018-05-19 12:33:30 -0700

    day 1

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,28 @@
 package section2;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		System.out.println("Ni Hao");
+		// JOptionPane.showMessageDialog(null, "Roblox is better");
+		Robot arnold = new Robot();
+		arnold.setSpeed(90);
+		arnold.setPenWidth(40);
+		arnold.sparkle();
+		arnold.arnold.setRandomPenColor();
+		arnold.penDown();
+		arnold.move(100);
+		arnold.turn(90);
+		arnold.move(100);
+		arnold.turn(90);
+		arnold.move(100);
+		arnold.turn(90);
+		arnold.move(100);
+		arnold.turn(90);
+		arnold.unSparkle();
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,24 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
-		// 5. Set your robot's pen down 
-		
+		Robot dad = new Robot();
+		// 5. Set your robot's pen down
+		dad.penDown();
 		// 3. Set the robot to go at max speed (10)
-		
+		dad.setSpeed(10);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i = 0; i < 175; i++) {
+			dad.move(100);
+			dad.turn(90);
+
 			// 7. Change the pen color to random
-		
+			dad.setRandomPenColor();
 			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-			
+			dad.move(5 * i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+			dad.turn(360 / 7);
 			// 8. Set the pen width to i
-			
+			dad.setPenWidth(i);
+		}
 	}
 }
