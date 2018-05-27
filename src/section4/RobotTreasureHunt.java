0979117012fc9b1ff5e28755fec7151a00346305
pigchick3n <League@administorsimac.attlocal.net>
2018-05-27 12:15:21 -0700

    Scary Maze, yeet

--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,10 +16,10 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+	
 	}
 
 	private void goDown() throws InterruptedException{
